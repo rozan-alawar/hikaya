efddd509e72348070b1a0a3509a01041307b1289
Rozan Alawar <rosanalawer2002@gmail.com>
2024-11-26 18:35:33 +0200

    create splash screen

---
+++ lib/app/utils/app_colors.dart
@@ -0,0 +1,28 @@
+
+
+// lib/app/core/values/app_colors.dart
+import 'package:flutter/material.dart';
+
+class AppColors {
+// ألوان رئيسية
+static const Color primary = Color(0xFF1B5E20);    // أخضر داكن
+static const Color accent = Color(0xFFF44336);     // أحمر
+static const Color background = Color(0xFFFAFAFA); // أبيض مائل للرمادي
+static const Color surface = Color(0xFFE3F2FD);    // أزرق فاتح جداً
+
+// ألوان النصوص
+static const Color textDark = Color(0xFF1F1F1F);   // أسود فاتح
+static const Color textLight = Color(0xFF757575);  // رمادي
+static const Color white = Colors.white;
+
+// ألوان إضافية
+static const Color error = Color(0xFFB00020);      // أحمر داكن
+static const Color success = Color(0xFF4CAF50);    // أخضر
+static const Color warning = Color(0xFFFFC107);    // أصفر
+
+// ألوان العلم الفلسطيني
+static const Color flagRed = Color(0xFFE4312B);    // أحمر العلم
+static const Color flagGreen = Color(0xFF149954);  // أخضر العلم
+static const Color flagBlack = Color(0xFF000000);  // أسود العلم
+static const Color flagWhite = Color(0xFFFFFFFF);  // أبيض العلم
+}
